7d14cc713c002dafef247369ef5ba04f97d16d50
DonMahallem <donmahallem@users.noreply.github.com>
2020-03-12 20:16:42 +0100

    push

--- packages/cli/src/heartrate-downloader.ts
+++ packages/cli/src/heartrate-downloader.ts
@@ -7,9 +7,9 @@ import { FlowDate } from '@abfluss/util';
 export class HeartRateDownloader {
 
     public downloadRange(startdate: string | FlowDate,
-        enddate: string | FlowDate,
-        samples: number,
-        throttle: number = 5000): void {
+                         enddate: string | FlowDate,
+                         samples: number,
+                         throttle: number = 5000): void {
         const convStartDate: FlowDate = (startdate instanceof FlowDate) ? startdate : FlowDate.fromString(startdate);
         const convEndDate: FlowDate = (enddate instanceof FlowDate) ? enddate : FlowDate.fromString(enddate);
         const downloadDates: FlowDate[] = FlowDate.toDateArray(convStartDate, convEndDate);
