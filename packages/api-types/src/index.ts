1ad98103b2d5608197da5bcd2b35ddb0e2ce56a0
donmahallem <donmahallem@users.noreply.github.com>
2020-03-12 16:22:08 +0100

    feat(api-types): removed duplicate methods (#28)
    Classes moved to package util

--- packages/api-types/src/index.ts
+++ packages/api-types/src/index.ts
@@ -2,7 +2,4 @@
  * Source https://github.com/abfluss/abfluss Package: api-types
  */
 
-export * from './flow-api-validator';
-export * from './summary-merger';
 export * from './model';
-export * from './flow-date';
