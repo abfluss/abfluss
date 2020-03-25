7e9591cbc4de648837199cb76d3747517def166e
donmahallem <donmahallem@users.noreply.github.com>
2020-03-25 10:39:57 +0100

    ci(github): update ci scripts

--- codecov_upload.sh
+++ codecov_upload.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-COVERAGE_FILES=(./packages/*/coverage/lcov.info)
+COVERAGE_FILES=(./packages/*/coverage/coverage-final.json)
+COVERAGE_FILES+=(./packages/*/coverage/**/coverage-final.json)
 echo "Found ${#COVERAGE_FILES[@]} Coverage Files"
 for coverage_filename in "${COVERAGE_FILES[@]}"
 do
@@ -7,6 +8,6 @@ do
     flag_name=$(echo "$package_name" | sed -r 's/(^|\W)(\w)/\U\2/g')
     echo "Uploading $package_name - $flag_name"
     echo "Coverage File: $coverage_filename"
-    bash <(curl -s https://codecov.io/bash) -F $flag_name -f $coverage_filename -n Unittest -y ./.codecov.yml
+    bash <(curl -s https://codecov.io/bash) -F $flag_name -f $coverage_filename -n $flag_name -y ./.codecov.yml
     echo "Uploaded"
 done
