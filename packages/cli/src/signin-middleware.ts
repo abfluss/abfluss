1632334d2a90109d80941ffc87f3b75d007d6fd6
ò_ó <donmahallem@users.noreply.github.com>
2021-07-24 09:08:37 +0200

    style(cli): use eslint config for styling (#437)

--- packages/cli/src/signin-middleware.ts
+++ packages/cli/src/signin-middleware.ts
@@ -1,14 +1,17 @@
-/*!
- * Source https://github.com/abfluss/abfluss Package: cli
+/*
+ * Package @abfluss/cli
+ * Source https://abfluss.github.io/abfluss/
  */
 
 import { FlowApiClient } from '@abfluss/api-client';
 import { Arguments, MiddlewareFunction } from 'yargs';
 export const createSigninMiddleware: (client: FlowApiClient) => MiddlewareFunction = (client: FlowApiClient): MiddlewareFunction => {
-    return (argv: Arguments & { email: string, password: string }): Promise<Response> => {
+    return (argv: Arguments & { email: string; password: string }): Promise<Response> => {
         console.log('Middleware called');
+        /* eslint-disable-next-line @typescript-eslint/no-unsafe-return, @typescript-eslint/no-explicit-any */
         return client.signin(argv.email, argv.password).then((val: any): any => {
             console.log('JJ', val);
+            /* eslint-disable-next-line @typescript-eslint/no-unsafe-return */
             return val;
         });
     };
